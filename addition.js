426bbedf729bb8f25adf92ecf8e00fedd3522399
ajay <103110610+ajay9595@users.noreply.github.com>
2022-06-13 13:33:25 +0530

    Update addition.js

--- addition.js
+++ addition.js
@@ -1,8 +1,8 @@
 function add()
 {
   
-var a = document.getElementById("firstno").value
-var b = document.getElementById("secondno").value
+var a = document.getElementById("firstnumber").value
+var b = document.getElementById("secondnumber").value
 var result
 result = parseInt(a)+parseInt(b)
 document.getElementById("result").value = result
