84f3ffcf50779685ef061be997d73881ed094bf8
ajay <103110610+ajay9595@users.noreply.github.com>
2022-06-13 18:13:06 +0530

    Update substraction.js

--- substraction.js
+++ substraction.js
@@ -1,7 +1,7 @@
 function substract()
 {
 var a = document.getElementById("firstnumber").value
-var b = document.getElementById("secondno").value
+var b = document.getElementById("secondnumber").value
 var result
 result = a-b
 document.getElementById("result").value = result
